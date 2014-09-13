7d31dac57ab934fa717402a0b4a499be54aa21e7
riverzhou <riverzhou2000@163.com>
2014-09-14 00:34:04 +0800

    调整一处日志格式

--- fd_worker/fd_sslclient.py
+++ fd_worker/fd_sslclient.py
@@ -388,12 +388,12 @@ class fd_client(pp_thread):
 
                 bid0 = fd_bid(self,0)
                 if bid0.do_bid() != True:
-                        printer.warning('client %s bid0 failed. Abort' % self.bidno)
+                        printer.warning('client %s bid 0 failed. Abort' % self.bidno)
                         return
 
                 bid1 = fd_bid(self,1)
                 if bid1.do_bid() != True:
-                        printer.warning('client %s bid1 failed. Abort' % self.bidno)
+                        printer.warning('client %s bid 1 failed. Abort' % self.bidno)
                         return
                
                 printer.warning('client %s bids finished. Quit' % self.bidno)
