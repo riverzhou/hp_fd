a799bd5a6cf94b49078cdd91fad0811aeb05de9b
riverzhou <riverzhou2000@163.com>
2014-11-02 12:24:05 +0800

    文件更民。fd_image -> fd_decode

--- fd_worker/fd_worker.py
+++ fd_worker/fd_worker.py
@@ -3,6 +3,7 @@
 from traceback              import print_exc
 from time                   import sleep
 
+from pp_baseredis           import pp_redis, pp_redis_init
 from pp_log                 import logger, printer
 
 from pp_global              import pp_global_info
@@ -10,7 +11,7 @@ from pp_server              import pp_dns_init
 from pp_config              import pp_config_init
 
 from fd_config              import redis_dbid
-from fd_image               import fd_image_init
+from fd_decode              import fd_decode_init
 from fd_channel             import fd_channel_init
 from fd_udpclient           import fd_udp_init
 from fd_synctime            import fd_timer_init
@@ -24,9 +25,11 @@ def main():
         global pp_global_info, printer
 
         pp_dns_init()
+        pp_redis_init()
+
         pp_config_init()
 
-        fd_image_init()
+        fd_decode_init()
         fd_channel_init()
         fd_udp_init()
         fd_timer_init()
