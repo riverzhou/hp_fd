ed98b5d6d277aa4bfd881adf08029144b0a19f7b
riverzhou <riverzhou2000@163.com>
2014-11-10 23:27:04 +0800

    将策略参数和账号参数的配置文件分离

--- fd_control/db05.py
+++ fd_control/db05.py
@@ -1,30 +1,29 @@
 #!/usr/bin/evn python3
 
+import  policy_f as policy
 
-# key_static  = 'cfg_static'
-# key_dynamic = 'cfg_dynamic'
-# key_trigger = 'cfg_trigger'
+#=============================================
+
+channel_trigger = policy.channel_trigger
+channel_timeout = policy.channel_timeout
+image_trigger   = policy.image_trigger
+decode_type     = policy.decode_type 
+decode_timeout  = policy.decode_timeout 
+decode_deadline = policy.decode_deadline 
+bid0_maxretry   = policy.bid0_maxretry 
+
+#=============================================
 
 account_list    = [
-        ('11111111', '1111', '张毅'),   # 测试
-        ('22222222', '2222', '张二'),   # 测试
-        ('33333333', '3333', '张三'),   # 测试
-        ('44444444', '4444', '张四'),   # 测试
-        ('55555555', '5555', '张五'),   # 测试
-        ('66666666', '6666', '张流'),   # 测试
-        ('77777777', '7777', '张琪'),   # 测试
-        ('88888888', '8888', '张吧'),   # 测试
-        ('99999999', '9999', '张九'),   # 测试
+        ('11111111', '1111', '张毅'),
+        ('22222222', '2222', '张二'),
+        ('33333333', '3333', '张三'),
+        ('44444444', '4444', '张四'),
+        ('55555555', '5555', '张五'),
+        ('66666666', '6666', '张流'),
+        ('77777777', '7777', '张琪'),
+        ('88888888', '8888', '张吧'),
+        ('99999999', '9999', '张九'),
         ]
 
-channel_trigger = [('10:31:10', '10:45:00') , ('11:28:10', '11:29:58')]
-channel_timeout = [110, 2]
-
-image_trigger   = [('10:35:00', 72600), ('11:29:40', 600), ('11:29:50', 400)]
-
-decode_type     = ['A', 'B', 'B']
-decode_timeout  = [30, 8, 6]
-decode_deadline = '11:29:57'
-
-bid0_maxretry   = 20
 
---
+++ fd_control/policy_e.py
@@ -0,0 +1,15 @@
+#!/usr/bin/evn python3
+
+channel_trigger = [('10:31:10', '10:45:00') , ('11:28:10', '11:29:58')]
+channel_timeout = [110, 2]
+
+bid0_maxretry   = 20
+
+#----------------------------------------------------------------------------
+
+image_trigger   = [('10:35:00', 72600), ('11:29:55', 300), (None, None)]
+
+decode_type     = ['A', 'C', 'C']
+decode_timeout  = [30, 2, 2]
+decode_deadline = '11:29:56'
+
