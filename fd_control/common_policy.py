639bdd079ea5a36ed74324c7f5c601bf4b2edc7d
riverzhou <riverzhou2000@163.com>
2014-12-02 11:44:59 +0800

    增加通道数量，预热时间提前到27:50

--- fd_control/common_policy.py
+++ fd_control/common_policy.py
@@ -1,7 +1,7 @@
 #!/usr/bin/evn python3
 
-channel_trigger = [('10:31:10', '10:45:00') , ('11:28:00', '11:29:58')]
-channel_timeout = [110, 2]
+channel_trigger = [('10:31:10', '10:45:00') , ('11:27:50', '11:29:58')]
+channel_timeout = [112, 2]
 
 bid0_maxretry   = 20
 
