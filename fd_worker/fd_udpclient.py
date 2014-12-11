8d5d0c9b850efc21512dfdfc66be62e99a58dfc1
riverzhou <riverzhou2000@163.com>
2014-12-11 19:32:08 +0800

    确认收盘的逻辑，增加一处异常判断

--- fd_worker/fd_udpclient.py
+++ fd_worker/fd_udpclient.py
@@ -156,7 +156,13 @@ class udp_worker(pp_thread):
 
         def check_game_over(self, cur_time):
                 global pp_global_info
-                if cur_time == None and pp_global_info.sys_code != None and time_sub(pp_global_info.sys_time, pp_global_info.deadline_decode) > 1:
+
+                try:
+                        deadline_decode = pp_global_info.deadline_decode
+                except:
+                        deadline_decode = '11:29:55'
+
+                if cur_time == None and pp_global_info.sys_code != None and time_sub(pp_global_info.sys_time, deadline_decode) > 1:
                         pp_global_info.set_game_over()
                         pp_global_info.event_price[0].set()
                         pp_global_info.event_price[1].set()
