b89c22b3c255e943cd9e5edb540089240b8a252c
riverzhou <riverzhou2000@163.com>
2015-02-02 21:11:31 +0800

    调整预热通道参数。调整上半场出价重试次数

--- fd_control/common_policy.py
+++ fd_control/common_policy.py
@@ -1,11 +1,11 @@
 #!/usr/bin/evn python3
 
-channel_trigger = [('10:32:30', '10:55:00') , ('11:27:45', '11:29:58')]
-channel_timeout = [114, 1.5]
+channel_trigger = [('10:32:30', '10:55:00') , ('11:27:40', '11:29:59')]
+channel_timeout = [116, 1.5]
 
 decode_deadline = '11:29:55'
 
-bid0_maxretry   = 20
+bid0_maxretry   = 30
 
 htmludp_addr    = ('192.168.1.90', 998)
 
