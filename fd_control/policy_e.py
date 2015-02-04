18b9d42f8b9950aa1696038733e7630e30863551
riverzhou <riverzhou2000@163.com>
2015-02-04 20:14:06 +0800

    调整上半场图片请求的超时时间设置

--- fd_control/policy_e.py
+++ fd_control/policy_e.py
@@ -6,6 +6,6 @@ image_trigger   = [('10:34:00', 73700), ('11:29:35', 800), ('11:29:55', 300)]
 
 decode_type     = ['A', 'A', 'C']
 decode_timeout  = [30, 20, 2]
-image_timeout   = [20, 7, 7]
+image_timeout   = [30, 7, 7]
 price_timeout   = [30, 2, 10]
 
