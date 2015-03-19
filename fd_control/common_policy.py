2706a22734184789562054607030ec484a9e97f9
riverzhou <riverzhou2000@163.com>
2015-03-19 21:14:50 +0800

    把htmludp 地址配置独立出来

--- fd_control/common_policy.py
+++ fd_control/common_policy.py
@@ -1,11 +1,11 @@
 #!/usr/bin/evn python3
 
+from htmludp_addr import htmludp_addr
+
 channel_trigger = [('10:32:30', '10:55:00') , ('11:27:40', '11:29:59')]
 channel_timeout = [116, 1.5]
 
 decode_deadline = '11:29:55'
 
 bid0_maxretry   = 30
-
-htmludp_addr    = ('192.168.1.90', 998)
 
