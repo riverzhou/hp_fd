eabb51933cac40432a23e0c48a25200ec17168bb
riverzhou <riverzhou2000@163.com>
2015-04-13 20:05:08 +0800

    通道的有效期，从116改成110

--- fd_control/common_policy.py
+++ fd_control/common_policy.py
@@ -3,7 +3,7 @@
 from htmludp_addr import htmludp_addr
 
 channel_trigger = [('10:32:30', '10:55:00') , ('11:27:40', '11:29:59')]
-channel_timeout = [116, 1.5]
+channel_timeout = [110, 1.5]
 
 decode_deadline = '11:29:55'
 
