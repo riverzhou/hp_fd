07d78199ab1d9c3c19830886f37bf088f133e09b
riverzhou <riverzhou2000@163.com>
2015-04-14 18:12:20 +0800

    更新日志分析的警示价到75200

--- fd_log/getlog.sh
+++ fd_log/getlog.sh
@@ -20,7 +20,7 @@ grep -H bid.aspx 	log/*info.log 	|grep ' 11:'		> 2_price_req.txt
 
 
 grep -H 'login' log/*warning.log 	|grep -v 'failed' 	> 0_login.txt
-grep -H 'bid 0 price 73700' log/*warning.log 			> 0_bidok.txt
+grep -H 'bid 0 price 75200' log/*warning.log 			> 0_bidok.txt
 grep -H 'bids finished' log/*warning.log 			> 0_result.txt
 grep -H -E '<ERRORCODE>112</ERRORCODE>' log/*time*		> 0_bid112.txt
 
