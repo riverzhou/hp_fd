ba2e192c8be4ca573a72c35a30425229b8c493d5
riverzhou <riverzhou2000@163.com>
2015-04-15 00:56:30 +0800

    解码deadline时间从55秒改成53秒

--- fd_control/common_policy.py
+++ fd_control/common_policy.py
@@ -5,7 +5,7 @@ from htmludp_addr import htmludp_addr
 channel_trigger = [('10:32:30', '10:55:00') , ('11:27:40', '11:29:59')]
 channel_timeout = [110, 1.5]
 
-decode_deadline = '11:29:55'
+decode_deadline = '11:29:53'
 
 bid0_maxretry   = 30
 
